fd0c6e6ec5879347930325d7c1fe529faefe218f
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 <svaddi10@asu.edu>
2023-11-27 10:25:04 +0000

    tests

--- robot/movement.py
+++ robot/movement.py
@@ -5,8 +5,8 @@ RightSig1 = 2
 RightSig2 = 3
 LeftSig1 = 4
 LeftSig2 = 17
-en1 = 14
-en2 = 15
+# en1 = 14
+# en2 = 15
 
 #setup
 GPIO.setmode(GPIO.BCM)
@@ -14,23 +14,24 @@ GPIO.setup(RightSig1, GPIO.OUT)
 GPIO.setup(RightSig2, GPIO.OUT)
 GPIO.setup(LeftSig1, GPIO.OUT)
 GPIO.setup(LeftSig2, GPIO.OUT)
-GPIO.setup(en1 , GPIO.OUT)
-GPIO.setup(en2 , GPIO.OUT)
-p1 = GPIO.PWM(en1, 1000)
-p2 = GPIO.PWM(en2 , 1000)
+# GPIO.setup(en1 , GPIO.OUT)
+# GPIO.setup(en2 , GPIO.OUT)
+# p1 = GPIO.PWM(en1, 1000)
+# p2 = GPIO.PWM(en2 , 1000)
 
 
 #time delay
 time.sleep(1)
-p1.start(50)
-p2.start(50)
+# p1.start(50)
+# p2.start(50)
 start_time = time.time()
 current = time.time()
-while current < start_time+10:
-    print(time.time())
+while True:
     GPIO.output(LeftSig1, GPIO.HIGH)
     GPIO.output(RightSig1, GPIO.HIGH)
     GPIO.output(LeftSig2, GPIO.LOW)
     GPIO.output(RightSig2, GPIO.LOW)
+    if time.time() - start_time() > 10:
+        break
     
 GPIO.cleanup()
