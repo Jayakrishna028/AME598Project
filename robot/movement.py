807c7f1f827c808629bb451cb38f3fd7e56fd4e0
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 <svaddi10@asu.edu>
2023-11-27 10:07:28 +0000

    tests

--- robot/movement.py
+++ robot/movement.py
@@ -5,16 +5,25 @@ RightSig1 = 2
 RightSig2 = 3
 LeftSig1 = 4
 LeftSig2 = 17
+en1 = 14
+en2 = 15
 
 #setup
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(RightSig1, GPIO.OUT, initial = GPIO.LOW)
-GPIO.setup(RightSig2, GPIO.OUT, initial = GPIO.LOW)
-GPIO.setup(LeftSig1, GPIO.OUT, initial = GPIO.LOW)
-GPIO.setup(LeftSig2, GPIO.OUT, initial = GPIO.LOW)
+GPIO.setup(RightSig1, GPIO.OUT)
+GPIO.setup(RightSig2, GPIO.OUT)
+GPIO.setup(LeftSig1, GPIO.OUT)
+GPIO.setup(LeftSig2, GPIO.OUT)
+GPIO.setup(en1 , GPIO.OUT)
+GPIO.setup(en2 , GPIO.OUT)
+p1 = GPIO.PWM(en1, 1000)
+p2 = GPIO.PWM(en2 , 1000)
+
 
 #time delay
 time.sleep(1)
+p1.start(25)
+p2.start(25)
 start_time = time.time()
 current = time.time()
 while current < start_time+10:
