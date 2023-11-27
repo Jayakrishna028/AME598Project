abafaec44617210e6b64086f7660d60b0ccb05f4
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 <svaddi10@asu.edu>
2023-11-27 21:21:32 +0000

    tests

--- robot/start.py
+++ robot/start.py
@@ -1,13 +1,18 @@
 import navigation
+import time
 import Distance
 import RPi.GPIO as GPIO
 
 GPIO.setmode(GPIO.BCM)
 
+GPIO.setup(14 , GPIO.OUT)
+GPIO.setup(15 , GPIO.IN)
+
 distanceFront = Distance.distance(14 , 15)
 
 while True:
     print(distanceFront)
+    time.sleep(0.5)
 
 
 GPIO.cleanup()
