568367a534226fa825a110b886d3e32c42d2ef5f
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 <svaddi10@asu.edu>
2023-11-27 09:52:15 +0000

    test

--- robot/movement.py
+++ robot/movement.py
@@ -1,26 +1,26 @@
 import RPi.GPIO as GPIO
 import time
 GPIO.cleanup()
-backRightSig1 = 2
-backRightSig2 = 3
-backLeftSig1 = 4
-backLeftSig2 = 17
+RightSig1 = 2
+RightSig2 = 3
+LeftSig1 = 4
+LeftSig2 = 17
 
 #setup
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(backRightSig1, GPIO.OUT, initial = GPIO.LOW)
-GPIO.setup(backRightSig2, GPIO.OUT, initial = GPIO.LOW)
-GPIO.setup(backLeftSig1, GPIO.OUT, initial = GPIO.LOW)
-GPIO.setup(backLeftSig2, GPIO.OUT, initial = GPIO.LOW)
+GPIO.setup(RightSig1, GPIO.OUT, initial = GPIO.LOW)
+GPIO.setup(RightSig2, GPIO.OUT, initial = GPIO.LOW)
+GPIO.setup(LeftSig1, GPIO.OUT, initial = GPIO.LOW)
+GPIO.setup(LeftSig2, GPIO.OUT, initial = GPIO.LOW)
 
 #time delay
 time.sleep(1)
 start_time = time.time()
 current = time.time()
 while current < start_time+10:
-    GPIO.output(backLeftSig1, GPIO.HIGH)
-    GPIO.output(backRightSig1, GPIO.HIGH)
-    GPIO.output(backLeftSig2, GPIO.LOW)
-    GPIO.output(backRightSig2, GPIO.LOW)
+    GPIO.output(LeftSig1, GPIO.HIGH)
+    GPIO.output(RightSig1, GPIO.HIGH)
+    GPIO.output(LeftSig2, GPIO.LOW)
+    GPIO.output(RightSig2, GPIO.LOW)
     
     
