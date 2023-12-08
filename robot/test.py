3e4dd72dc2edc02179f1ef6cf2db898d80da7eef
Jayakrishna028 <svaddi10@asu.edu>
2023-12-07 22:31:08 -0700

    final tests:

--- robot/test.py
+++ robot/test.py
@@ -1,10 +1,7 @@
 from http_request import serverRequest
-import Distance
 
 serverRequest(23, 33, 23)
 serverRequest(23, 24, 54)
 serverRequest(25, 23, 53)
 serverRequest(20, 23, 64)
 
-while True:
-    Distance.distance()
